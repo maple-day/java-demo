b651648f3014422458266d77367465d9391a9510
itg <896757055@qq.com>
2020-01-13 16:50:28 +0800

    maven 插件

---
+++ maven-plugin/maven-plugin-demo/src/main/java/com/fangda/mojo/MyTestMojo.java
@@ -0,0 +1,24 @@
+package com.fangda.mojo;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+/**
+ * @ProjectName: java-demo
+ * @Package: com.fangda.mojo
+ * @ClassName: MyTestMojo
+ * @Author: ccq
+ * @Description: 测试01maven插件
+ * @Date: 2020/1/13 16:24
+ * @eg mvn groupId:artifactId:version:goal
+ *      mvn com.fangda:maven-plugin-demo:1.0-SNAPSHOT:myTest
+ */
+@Mojo(name = "myTest")
+public class MyTestMojo extends AbstractMojo {
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().info("hello world maven-plugin");
+
+    }
+}
